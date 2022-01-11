355e6a5d1da1e697ec826f410b58e7cc515b9f3e
Serhii Biliaiev <ocnskyy@gmail.com>
2022-01-11 18:19:42 +0200

    add user model

---
+++ src/main/java/com/userManagement/UserModel.java
@@ -0,0 +1,49 @@
+package com.userManagement;
+
+import java.util.Objects;
+
+public class UserModel {
+    private int id;
+    private String name;
+    private String email;
+
+    public UserModel(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return id == userModel.id && Objects.equals(name, userModel.name) && Objects.equals(email, userModel.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
